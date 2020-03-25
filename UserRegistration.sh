53f5172808a133d609a9e92211b374d5db0c31ef
ruchika <ruchika6698@gmail.com>
2020-03-25 20:03:07 +0530

    Added:Validate Email Address

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -6,23 +6,41 @@ echo "Enter Your First Name :"
 read FirstName
 echo "Enter Your Last Name :"
 read LastName
-
-
+echo "Enter Youe Email Address :"
+read EmailAddress
 
 fname1="^[A-Z][a-zA-Z]{3,15}$"
-
-if [[ $FirstName =~ $fname1 ]]
-then
-	echo yes;
-else
-	echo No;
-fi
-
 lname1="^[A-Z][a-zA-Z]{3,15}$"
+email="^([0-9a-zA-Z]+)([_+-.][0-9a-zA-Z])?[@][0-9a-zA-Z]+([.][a-z]{2,4})([.][a-z]{2})?$"
 
-if [[ $LastName =~ $lname1 ]]
-then
-   echo yes;
-else
-   echo No;
-fi
+function firstname()
+{
+	if [[ $FirstName =~ $fname1 ]]
+	then
+		echo "First Name is Valid";
+	else
+		echo "First Name is Invalid";
+	fi
+}
+
+function lastname()
+{
+	if [[ $LastName =~ $lname1 ]]
+	then
+   	echo "Last Name is Valid";
+	else
+   	echo "Last Name is Invalid";
+	fi
+}
+
+function emailId()
+{
+	if [[ $EmailAddress =~ $email ]]
+	then
+		echo "Email Address is Valid";
+	else
+		echo "Email Address is Invalid";
+	fi
+}
+emailId
+
