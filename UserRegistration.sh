090d551a7c9b7f988cc5bfc2d899815e18589af4
ruchika <ruchika6698@gmail.com>
2020-03-25 19:06:14 +0530

    Added:Validate First Name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,3 +1,15 @@
 #!/bin/bash -x
 
 printf "Please fill the User details and Registration \n"
+
+echo "Enter Your First Name"
+read FirstName
+
+fname1="^[A-Z][a-zA-Z]{3,15}$"
+
+if [[ $FirstName = ~$fname1 ]]
+then
+	echo yes;
+else
+	echo No;
+fi
