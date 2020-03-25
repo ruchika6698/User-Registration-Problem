7fe2a3fb77de2b2836b3e5baf502f8cdf5d530da
ruchika <ruchika6698@gmail.com>
2020-03-25 19:15:18 +0530

    Added:Validate Last Name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -2,14 +2,27 @@
 
 printf "Please fill the User details and Registration \n"
 
-echo "Enter Your First Name"
+echo "Enter Your First Name :"
 read FirstName
+echo "Enter Your Last Name :"
+read LastName
+
+
 
 fname1="^[A-Z][a-zA-Z]{3,15}$"
 
-if [[ $FirstName = ~$fname1 ]]
+if [[ $FirstName =~ $fname1 ]]
 then
 	echo yes;
 else
 	echo No;
+fi
+
+lname1="^[A-Z][a-zA-Z]{3,15}$"
+
+if [[ $LastName =~ $lname1 ]]
+then
+   echo yes;
+else
+   echo No;
 fi
